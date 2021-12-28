2c3651981b8c425206749ea83b336c305c4bf278
Edward Jo <helloedwardjo@gmail.com>
2021-12-28 15:06:14 +0900

    Update main screen     - Start App after Singleton services are initialized     - Add action buttons to back up, restore, and delete messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,55 @@
+import 'dart:async';
 import 'dart:developer' as developer;
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'models/message.dart';
 import 'services/service_locator.dart';
 import 'view_models/message_viewmodel.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: serviceLocator.allReady(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(
+            child: CircularProgressIndicator(strokeWidth: 2.0),
+          );
+        }
+
+        if (snapshot.hasError) {
+          showDialog(
+              context: ctx,
+              builder: (_) {
+                return CupertinoAlertDialog(
+                  content: Text(snapshot.error.toString()),
+                );
+              });
+          return Container();
+        }
+
+        return ChangeNotifierProvider.value(
+          value: serviceLocator<MessageViewModel>(),
+          child: const StartMyApp(),
+        );
+      },
+    );
+  }
+}
+
+class StartMyApp extends StatelessWidget {
+  const StartMyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -37,14 +75,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final MessageViewModel _model = serviceLocator<MessageViewModel>();
-  final _messageController = TextEditingController();
-  late Future<bool> _readMessagesFuture;
+  late final MessageViewModel _model;
+
+  StreamSubscription? _backupProgressSub, _restoreProgressSub;
+  bool _isUploading = false;
+  bool _isDownloading = false;
 
   @override
   void initState() {
-    _readMessagesFuture = _model.readAll();
+    _model = context.read<MessageViewModel>();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (_isUploading && _backupProgressSub != null) {
+      _backupProgressSub!.cancel();
+    }
+
+    if (_isDownloading && _restoreProgressSub != null) {
+      _restoreProgressSub!.cancel();
+    }
+
+    super.dispose();
   }
 
   @override
@@ -53,91 +106,207 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          IconButton(onPressed: () {}, icon: const Icon(Icons.cloud_upload)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.cloud_download)),
+          IconButton(
+            onPressed: _backupMessages,
+            icon: const Icon(Icons.cloud_upload),
+          ),
+          IconButton(
+            onPressed: _restoreMessages,
+            icon: const Icon(Icons.cloud_download),
+          ),
+          IconButton(
+            onPressed: _deleteMessages,
+            icon: const Icon(Icons.delete),
+          ),
         ],
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              // MESSAGE LIST
-              Expanded(
-                child: FutureBuilder(
-                  future: _readMessagesFuture,
-                  builder: (ctx, snapshot) {
-                    if (snapshot.connectionState != ConnectionState.done) {
-                      return const SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: CircularProgressIndicator(strokeWidth: 2.0),
-                      );
-                    }
-
-                    if (snapshot.hasError) {
-                      CupertinoAlertDialog(
-                        content: Text(snapshot.error.toString()),
-                      );
-                      return Container();
-                    }
-
-                    return ListView.builder(
-                        itemCount: _model.messages.length,
-                        itemBuilder: (_, i) {
-                          return MessageListItem(
-                            message: _model.messages[i].message,
-                            date: _model.messages[i].createdTime.toString(),
-                          );
-                        });
-                  },
-                ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tightFor(
+                height: constraints.maxHeight,
               ),
-              // TEXT INPUT & SAVE
-              Container(
-                color: const Color(0xFFEBECED),
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: IntrinsicHeight(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextField(
-                      maxLines: 1,
-                      controller: _messageController,
-                      decoration: const InputDecoration(
-                        // labelText: 'New message',
-                        hintText: 'New message',
-                        hintStyle: TextStyle(fontSize: 12),
-                      ),
-                      onSubmitted: null,
-                    ),
-                    TextButton(
-                      onPressed: () => _save(context),
-                      child: const Text('Save'),
-                    ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const <Widget>[
+                    MessageList(),
+                    MessageTextInput(),
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
 
-  Future _save(BuildContext context) async {
-    developer.log('Save message( ${_messageController.text.trim()} )');
-    await _model.save(
-      Message(
-        id: null,
-        message: _messageController.text.trim(),
-        createdTime: DateTime.now(),
-      ),
+  Future<void> _backupMessages() async {
+    Future? progressDialog = Dialog.progressDialog(
+      context,
+      'Backup in progress',
     );
-    setState(() {});
-    _messageController.text = '';
-    FocusScope.of(context).unfocus();
-    return;
+
+    await _model.uploadMessages((Stream<double> stream) async {
+      void Function(double) onData;
+      void Function() onDone;
+      Function onError;
+
+      onData = (progress) => developer.log('UPLOAD PROGRESS: $progress');
+
+      onDone = () {
+        developer.log('UPLOAD COMPLETED');
+        if (progressDialog != null) {
+          Navigator.of(context).pop();
+          progressDialog = null;
+        }
+        _isUploading = false;
+      };
+
+      onError = (err) {
+        developer.log('Failed to upload file by error($err)');
+        if (progressDialog != null) {
+          Navigator.of(context).pop();
+          progressDialog = null;
+        }
+        _isUploading = false;
+      };
+
+      _isUploading = true;
+
+      _backupProgressSub = stream.listen(
+        onData,
+        onDone: onDone,
+        onError: onError,
+        cancelOnError: true,
+      );
+    });
+  }
+
+  Future<void> _restoreMessages() async {
+    Future? progressDialog = Dialog.progressDialog(
+      context,
+      'Restore in progress',
+    );
+
+    await _model.downloadMessages((Stream<double> stream) async {
+      void Function(double) onData;
+      void Function() onDone;
+      Function onError;
+
+      onData = (progress) => developer.log('DOWNLOAD PROGRESS: $progress');
+
+      onDone = () async {
+        developer.log('DOWNLOAD COMPLETED');
+        if (progressDialog != null) {
+          Navigator.of(context).pop();
+          progressDialog = null;
+        }
+        _isDownloading = false;
+        await _model.reloadMessages();
+      };
+
+      onError = (err) {
+        developer.log('Failed to download file by error($err)');
+        if (progressDialog != null) {
+          Navigator.of(context).pop();
+          progressDialog = null;
+        }
+        _isDownloading = false;
+      };
+
+      _isDownloading = true;
+
+      _restoreProgressSub = stream.listen(
+        onData,
+        onDone: onDone,
+        onError: onError,
+        cancelOnError: true,
+      );
+    });
+  }
+
+  Future<void> _deleteMessages() async {
+    Dialog.progressDialog(
+      context,
+      'Deleting...',
+    );
+    await _model.deleteMessages();
+    Future.delayed(
+      const Duration(seconds: 1),
+      () async {
+        Navigator.of(context).pop();
+        await _model.reloadMessages();
+      },
+    );
+  }
+}
+//------------------------------------------------------------------------------
+// Message List
+//------------------------------------------------------------------------------
+
+class MessageList extends StatefulWidget {
+  const MessageList({Key? key}) : super(key: key);
+
+  @override
+  _MessageListState createState() => _MessageListState();
+}
+
+class _MessageListState extends State<MessageList> {
+  late Future<bool> _readMessagesFuture;
+
+  @override
+  void initState() {
+    _readMessagesFuture = context.read<MessageViewModel>().readAll();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _readMessagesFuture,
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const SizedBox(
+            width: 20,
+            height: 20,
+            child: CircularProgressIndicator(strokeWidth: 2.0),
+          );
+        }
+
+        if (snapshot.hasError) {
+          showDialog(
+              context: ctx,
+              builder: (_) {
+                return CupertinoAlertDialog(
+                  content: Text(snapshot.error.toString()),
+                );
+              });
+          return Container();
+        }
+
+        return Expanded(
+          child: SizedBox(
+            child: ListView.builder(
+                // itemCount: messages.length,
+                itemCount: context.watch<MessageViewModel>().messages.length,
+                itemBuilder: (_, i) {
+                  return MessageListItem(
+                    message:
+                        context.watch<MessageViewModel>().messages[i].message,
+                    date: context
+                        .watch<MessageViewModel>()
+                        .messages[i]
+                        .createdTime
+                        .toString(),
+                  );
+                }),
+          ),
+        );
+      },
+    );
   }
 }
 
@@ -178,6 +347,107 @@ class MessageListItem extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+//------------------------------------------------------------------------------
+// Message Input
+//------------------------------------------------------------------------------
+
+class MessageTextInput extends StatefulWidget {
+  const MessageTextInput({Key? key}) : super(key: key);
+
+  @override
+  State<MessageTextInput> createState() => _MessageTextInputState();
+}
+
+class _MessageTextInputState extends State<MessageTextInput> {
+  final _messageController = TextEditingController();
+
+  late final MessageViewModel _model;
+
+  @override
+  void initState() {
+    _model = context.read<MessageViewModel>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFFEBECED),
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            maxLines: 1,
+            controller: _messageController,
+            decoration: const InputDecoration(
+              // labelText: 'New message',
+              hintText: 'New message',
+              hintStyle: TextStyle(fontSize: 12),
+            ),
+            onSubmitted: null,
+          ),
+          TextButton(
+            onPressed: () => _save(context),
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future _save(BuildContext context) async {
+    developer.log('Save message( ${_messageController.text.trim()} )');
+    await _model.save(
+      Message(
+        id: null,
+        message: _messageController.text.trim(),
+        createdTime: DateTime.now(),
+      ),
+    );
+    // setState(() {});
+    _messageController.text = '';
+    FocusScope.of(context).unfocus();
+    return;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Dialog
+//------------------------------------------------------------------------------
+class Dialog {
+  static Future progressDialog(BuildContext context, String message) {
+    Widget dialogContent = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(
+          width: 20,
+          height: 20,
+          child: CircularProgressIndicator(strokeWidth: 2.0),
+        ),
+        const SizedBox(height: 10),
+        Text(
+          message,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: Theme.of(context).textTheme.bodyText2!.fontSize),
+        ),
+      ],
+    );
+
+    Widget dialog = (Platform.isIOS)
+        ? CupertinoAlertDialog(content: dialogContent)
+        : AlertDialog(content: dialogContent);
+
+    return showDialog(
+      context: context,
+      builder: (_) {
+        return WillPopScope(child: dialog, onWillPop: () async => false);
+      },
     );
   }
 }
