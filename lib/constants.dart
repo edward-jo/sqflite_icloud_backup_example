1e94915181cd1de8ef981365cecb9745ded2242d
Edward Jo <helloedwardjo@gmail.com>
2021-12-28 14:39:41 +0900

    Update constant variables     - Add constant for database backup file name     - Add constant for icloud connection error

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 const String appDatabaseFileName = 'app.db';
+const String appDatabaseBackupFileName = 'app.db.backup';
 const int appDatabaseVersion = 1;
 
 const String messagesTableName = 'messages';
@@ -7,4 +8,9 @@ class MessagesDbCols {
   static const String id = '_id';
   static const String message = 'message';
   static const String createdTime = 'created_time';
+}
+
+class ErrorMessages {
+  static const String iCloudConnectionOrPermissionStr =
+      'Platform Exception: iCloud container ID is not valid, or user is not signed in for iCloud, or user denied iCloud permission for this app';
 }
