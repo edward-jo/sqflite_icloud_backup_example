71658fccba2c5e3c7c2e7808c5e4f1ff9c25a23e
Edward Jo <helloedwardjo@gmail.com>
2021-12-28 14:46:07 +0900

    Change MessageViewModel class to one extends ChangedNotifier     - Implement methods to upload, download, and delete message

--- lib/view_models/message_viewmodel.dart
+++ lib/view_models/message_viewmodel.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
 import 'dart:developer' as developer;
+import 'package:flutter/material.dart';
+import '../constants.dart';
 import '../models/message.dart';
+import '../services/icloud_storage/icloud_storage_service.dart';
 import '../services/app_database/app_database_service.dart';
 import '../services/service_locator.dart';
 
-class MessageViewModel {
+class MessageViewModel extends ChangeNotifier {
   final AppDatabaseService _dbService = serviceLocator<AppDatabaseService>();
+  final IcloudStorageService _iStorageService =
+      serviceLocator<IcloudStorageService>();
   List<Message> _messages = [];
 
   List<Message> get messages => _messages;
@@ -12,6 +18,7 @@ class MessageViewModel {
   Future<bool> save(Message message) async {
     Message msg = await _dbService.createMessage(message);
     _messages.add(msg);
+    notifyListeners();
     return true;
   }
 
@@ -21,6 +28,48 @@ class MessageViewModel {
       developer.log(msg.toJson().toString());
     }
     _messages = all;
+    notifyListeners();
     return true;
+  }
+
+  Future<void> uploadMessages(
+    void Function(Stream<double>) onProgress,
+  ) async {
+    final path = await _dbService.getAppDatabaseFilePath();
+    await _iStorageService.uploadFile(
+      path,
+      appDatabaseBackupFileName,
+      onProgress,
+    );
+  }
+
+  Future<void> downloadMessages(
+    void Function(Stream<double>) onProgress,
+  ) async {
+    final path = await _dbService.getAppDatabaseFilePath();
+
+    await _dbService.closeAppDatabase();
+    await _dbService.deleteAppDatabase();
+
+    await _iStorageService.downloadFile(
+      appDatabaseBackupFileName,
+      path,
+      onProgress,
+    );
+
+    _messages.clear();
+    notifyListeners();
+  }
+
+  Future<void> deleteMessages() async {
+    await _dbService.closeAppDatabase();
+    await _dbService.deleteAppDatabase();
+    _messages.clear();
+    notifyListeners();
+  }
+
+  Future<void> reloadMessages() async {
+    await _dbService.init();
+    await readAll();
   }
 }
